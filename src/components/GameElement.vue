a3402359b52f3eebb3b26e481a4714f68509538a
kurehajime <xiidec@gmail.com>
2023-01-05 23:21:27 +0900

    ますめ

--- src/components/GameElement.vue
+++ src/components/GameElement.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue';
 import BallElement from './BallElement.vue';
+import GridElement from './GridElement.vue';
 import { useQuartz } from '../hooks/useQuartz'
 import { Pong } from '../models/Pong';
 import { Setting } from '../models/Setting';
 const [getTime, start] = useQuartz(1000);
 const time = ref(0);
 const setting = {
-  width: 500,
+  width: 550,
   height: 600,
   startPoint: { x: 50, y: 60 },
   startVec: { x: 3, y: 3 },
@@ -26,15 +27,14 @@ watchEffect(() => {
 </script>
 
 <template>
-  <svg>
-    <rect x="0" y="0" width="500" height="600" fill="black" stroke="gray" stroke-width="10" />
+  <svg :width="setting.width" :height="setting.height">
+    <rect x="0" y="0" :width="setting.width" :height="setting.height" fill="black" />
+    <GridElement :setting="setting" />
+    <rect x="0" y="0" width="550" height="600" fill="transparent" stroke="gray" stroke-width="2" />
     <BallElement :x="pong.ball.x" :y="pong.ball.y" />
   </svg>
 </template>
 
 <style scoped>
-svg {
-  width: 500px;
-  height: 600px;
-}
+
 </style>
---
+++ src/components/GridElement.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">import { Setting } from '../models/Setting';
+
+type Props = {
+    setting: Setting,
+}
+const props = defineProps<Props>()
+const yoko: number[] = []
+const tate: number[] = []
+for (let i = 0; i < props.setting.width; i += 50) {
+    yoko.push(i)
+}
+for (let i = 0; i < props.setting.height; i += 50) {
+    tate.push(i)
+}
+
+</script>
+
+<template>
+    <g>
+        <g v-for="x in yoko" :key="x">
+            <line :x1="x" y1="0" :x2="x" :y2="setting.height" stroke="#111111" />
+        </g>
+        <g v-for="y in tate" :key="y">
+            <line x1="0" :y1="y" :x2="setting.width" :y2="y" stroke="#111111" />
+        </g>
+    </g>
+</template>
+
+<style scoped>
+
+</style>
